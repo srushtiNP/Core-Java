88b9867eca31ae9636aa19a79e6665c34fc22341
srushti pattanashetti <srushtipattanashetti.xworkz@gmail.com>
2023-06-20 13:58:17 -1000

    Test-push

---
+++ datatypes.java
@@ -0,0 +1,47 @@
+class student{
+public static void main(String args[]){
+	int totalNumberOfStudent=20;
+	String companyName="Xworkz";
+	int numberOfEmployees = 22;
+	String nameOfCollege = "BGMIT";
+	int numberOfLaptops = 30;
+	String nameOfLaptop = "dell";
+	int numberOfCars=35;
+	String colourName="red";
+	byte numberOfDaysinAweek =7;
+	byte numberOfMonthsinAyear=12;
+	byte numberOfColoursinArainbow=7;
+	byte numberOfDirections=6;
+	byte sidesOfSquare=4;
+	byte numberOfMinutesinaHour=60;
+	byte numberOfVowels=5;
+	byte numberOfPlanentsinAgalaxy=9;
+	byte numberOfTalukinABagalkot=7;
+	byte numberOfDistrictsinAKarnataka=38;
+	short feesofprimaryschool=30000;
+	short prizeofgoldgm=4000;
+	short strengthofclg=1500;
+	
+	System.out.println(totalNumberOfStudent);
+	System.out.println(companyName);
+	System.out.println(numberOfEmployees);
+	System.out.println(nameOfCollege);
+	System.out.println(numberOfLaptops);
+	System.out.println(numberOfCars);
+	System.out.println(colourName);
+	System.out.println(numberOfDaysinAweek);
+	System.out.println(numberOfMonthsinAyear);
+	System.out.println(numberOfColoursinArainbow);
+	System.out.println(numberOfDirections);
+	System.out.println(sidesOfSquare);
+	System.out.println(numberOfMinutesinaHour);
+	System.out.println(numberOfVowels);
+	System.out.println(numberOfPlanentsinAgalaxy);
+	System.out.println(numberOfTalukinABagalkot);
+	System.out.println(numberOfDistrictsinAKarnataka);
+	System.out.println(feesofprimaryschool);
+	System.out.println(prizeofgoldgm);
+	System.out.println(strengthofclg);
+	
+}
+}
