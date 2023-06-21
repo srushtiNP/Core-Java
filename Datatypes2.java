70490d58fd32398e2ecf18c921b950cc54a615da
srushti pattanashetti <srushtipattanashetti.xworkz@gmail.com>
2023-06-20 23:02:02 -1000

    Datatypepush

---
+++ Datatypes2.java
@@ -0,0 +1,93 @@
+public class Datatypes{
+	public static void main(String[] args){
+		//byte 
+		byte age = 25;
+		byte temperature=-10;
+		byte numberOfSubjects=6;
+		byte daysInWeek = 7;
+        byte playersInTeam = 11;
+        byte booksOnShelf = 50;
+        byte gameScore = 100;
+        byte numberOfSiblings = 2;
+        byte colorsInRainbow = 7;
+        byte maxLoginAttempts = 3;
+		//short
+		short populationInAVillage = 15000;
+        short distance = 5000;
+        short priceOfAShirt = 500;
+        short revenue = 1000;
+        short balance = 2500;
+        short stock = 100;
+        short year = 2023;
+        short maxSpeed = 200;
+        short ageLimit = 18;
+        short capacity = 100;
+		//int
+		int populationDensity = 100;
+        int daysRemaining = 30;
+        int price = 500;
+        int quantity = 10;
+        int area = 2500;
+        int volume = 1000;
+        int roomNumber = 101;
+        int pages = 500;
+        int seatsAvailable = 50;
+        int employeeCount = 100;
+		//long
+		long worldPopulation = 7800000000L;
+        long nationalDebt = 2500000000000L;
+        long accountBalance = 1000000L;
+        long mobileNumber = 1234567890L;
+        long distanceTravelled = 500000L;
+        long fileSize = 2000000L;
+        long timestamp = 1623838763000L;
+        long itemCode = 1234567890L;
+        long creditCardNumber = 9876543210L;
+        long referenceNumber = 1234567890L;
+		//float
+		float weight = 68.5f;
+        float height = 165.75f;
+        float temperatureCelsius = 25.5f;
+        float pricePerUnit = 10.5f;
+        float discountRate = 0.2f;
+        float exchangeRate = 1.5f;
+        float GPA = 3.8f;
+        float taxRate = 0.15f;
+        float humidityLevel = 0.6f;
+        float interestRate = 4.5f;
+		//double
+		double pi = 3.14159;
+        double salary = 2500.50;
+        double areaCircle = 314.16;
+        double bankBalance = 50000.75;
+        double latitude = 12.3456789;
+        double longitude = -98.7654321;
+        double weightKg = 75.5;
+        double heightCm = 180.25;
+        double distanceMiles = 50.5;
+        double temperatureFahrenheit = 98.6;
+		//char
+		char gender = 'M';
+        char alphabetC = 'C';
+		char punctuationComma = ',';
+		char grade = 'A';
+        char hash = '#';
+		char dollarSymbol = '$';
+		char digit = '0';
+		char semicolon = ':';
+		char addSymbol = '+';
+		char bracket='(';
+	
+		//boolean
+		boolean isRaining = false;
+        boolean isSunny = true;
+        boolean isWeekend = true;
+		boolean is5Even = false;
+		boolean isItJanuary = false;
+		boolean isIt2025 = false;
+		boolean amIGod = false;
+		boolean amIAlive = true;
+		boolean isItDaytime = true;
+		boolean isItNighttime = false;
+	}
+}
