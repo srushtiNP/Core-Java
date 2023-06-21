a25c9ace1dce8118d5a7b4d0afe03fd24796a0be
srushti pattanashetti <srushtipattanashetti.xworkz@gmail.com>
2023-06-20 22:25:39 -1000

    variablepush

---
+++ VariableClass.java
@@ -0,0 +1,34 @@
+public class VariableClass{
+	public static void main(String[] args){
+		
+	int totalNumberOfStudents = 20;
+	System.out.println(totalNumberOfStudents);
+	
+	String nameOfCompany = "Xworks";
+	System.out.println(nameOfCompany);
+	
+	int numberOfEmployees = 50;
+	System.out.println(numberOfEmployees);
+	
+	char divisionOfCompany = 'A';
+	System.out.println(divisionOfCompany);
+	
+	int numberOfClasses = 2;
+	System.out.println(numberOfClasses);
+	
+	String nameOfTrainee = "SAKSHI";
+	System.out.println(numberOfClasses);
+	
+	byte workingDaysOfCompany = '7';
+	System.out.println(workingDaysOfCompany);
+	
+	String modeOfTraining = "Online";
+	System.out.println(modeOfTraining);
+	
+	boolean isMondayWorkingDay = true;
+	System.out.println(isMondayWorkingDay);
+	
+	int batchStrenght = 28;
+	System.out.println(batchStrenght);
+}
+}
