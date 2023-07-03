be9dcd268332b89e486a5fc43c434e9265211378
srushti pattanashetti <srushtipattanashetti.xworkz@gmail.com>
2023-07-02 22:16:34 -1000

    pushPendrive

---
+++ Pendrive.java
@@ -0,0 +1,74 @@
+public class Pendrive{
+	String nameOfPendrive;
+	int price;
+	String colour;
+	String shape;
+	String size;
+	int storageCapacity;
+	float power;
+	int dataTransfer;
+	String material;
+	String brand;
+    String compatibility;
+	String hardwareInterface;
+	float readSpeed;
+	int warranty;
+	float weight;
+	String operatingSystem;
+	int thickness;
+	String connectorType;
+	int numberOfPorts;
+	float generation;
+	
+	public static void main(String[] args)
+	{
+		Pendrive obj = new Pendrive(1077,"red");
+	}
+	
+	public Pendrive(int price, String colour){
+		this.price=price;
+		this.colour=colour;
+		System.out.println(price);
+		System.out.println(colour);
+	}
+	
+	
+	public static void transferImages(){
+		
+	}
+	public static void storeFiles(){
+		
+	}
+	public static void applicationInstallation(){
+		
+	}
+	public static void installationOfOperatingSystem(){
+		
+	}
+	public static void dataBackup(){
+		
+	}
+	public static void transferLargeVedios(){
+		
+	}
+	public static void copy(){
+		
+	}
+	public static void paste(){
+		
+	}
+	public static void unlockCloudDataStorage(){
+		
+	}
+	public static void readAndWrite(){
+		
+	}
+	 
+	 
+	 
+	
+	
+	
+	
+	
+}
